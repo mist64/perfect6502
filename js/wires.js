ca5fe121d5bd82001e2baa7b61c7cb7c4d67afd0
Michael Steil <mist64@mac.com>
2010-09-21 05:14:55 +0000

    remove all graphics

--- js/wires.js
+++ js/wires.js
@@ -68,12 +68,10 @@ function setupNodes(){
 	for(var i in segdefs){
 		var seg = segdefs[i];
 		var w = seg[0];
-		if(nodes[w]==undefined) 
-			nodes[w] = {segs: new Array(), num: w, pullup: seg[1]=='+',
-			            state: 'fl', gates: new Array(), c1c2s: new Array()};
+		nodes[w] = {pullup: seg[1]=='+',
+		            state: 'fl', gates: new Array(), c1c2s: new Array()};
 		if(w==ngnd) continue;
 		if(w==npwr) continue;
-		nodes[w].segs.push(seg.slice(3));
 	}
 }
 
